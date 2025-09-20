478fba1344c43332b10d194e0b77f68b896e6f20
LordKymzul <kymkymzul@gmail.com>
2025-09-20 20:30:38 +0800

    a

--- app/(routes)/(unprotected)/layout.tsx
+++ app/(routes)/(unprotected)/layout.tsx
@@ -1,5 +1,12 @@
+import HeadNav from "@/src/core/shared/presentation/components/head-nav";
+import SideBar from "@/src/core/shared/presentation/components/side-bar";
+
 const UnprotectedLayout = ({ children }: { children: React.ReactNode }) => {
-    return <div>{children}</div>;
+    return (
+        <div className="flex h-screen overflow-hidden">
+           
+        </div>
+    )
 };
 
 export default UnprotectedLayout;
